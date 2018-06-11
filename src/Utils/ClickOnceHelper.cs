4b27e40ae56b7a5a40962cd8c49b529d7e59044c
Loïc Wolff <loicwolff@gmail.com>
2018-06-11 09:28:40 +0200

    changed some string building

--- src/Utils/ClickOnceHelper.cs
+++ src/Utils/ClickOnceHelper.cs
@@ -10,14 +10,12 @@ namespace ClipboardManager
         {
             if (ApplicationDeployment.IsNetworkDeployed && ApplicationDeployment.CurrentDeployment.IsFirstRun)
             {
-                string startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
-                startupPath = Path.Combine(startupPath, productName) + ".appref-ms";
+                string startupPath = $"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), productName)}.appref-ms";
 
                 if (!File.Exists(startupPath))
                 {
-                    string allProgramsPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
+                    string shortcutPath = $"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Programs), publisherName, productName)}.appref-ms";
                     
-                    string shortcutPath = Path.Combine(allProgramsPath, publisherName, productName) + ".appref-ms";
                     if (File.Exists(shortcutPath))
                     {
                         File.Copy(shortcutPath, startupPath);
