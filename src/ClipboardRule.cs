9d61af0e304cd2d82e47cb27238d0af7bfa38c4d
Loïc Wolff <loicwolff@gmail.com>
2019-09-08 17:33:07 +0200

    Added guid + PR rules Improved redmine rule

--- src/ClipboardRule.cs
+++ src/ClipboardRule.cs
@@ -2,18 +2,16 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
     using System.Text.RegularExpressions;
 
-    [DebuggerDisplay("Type: {Type}")]
     public class ClipboardRule
     {
         public static IEnumerable<ClipboardRule> GetMatchingRules(string text)
         {
             if (!String.IsNullOrWhiteSpace(text))
             {
-                foreach (ClipboardRule rule in ClipboardRules)
+                foreach (var rule in ClipboardRules)
                 {
                     if (rule.IsMatch(text, out string[] values))
                     {
@@ -33,12 +31,18 @@
             new ClipboardRule
             {
                 Label = "Redmine issue",
+                /*language=regex*/
                 RegexPattern = @"
                     ^
                     \s*
-                    (?:refs)?
-                    \s*
-                    \#([0-9]+)
+                    (?i:
+                        refs\s?\#?
+                        |
+                        RM\s?
+                        |
+                        (?:\#)
+                    )
+                    ([0-9]+)
                     \s*
                     $
                     ",
@@ -53,6 +57,56 @@
                     }
                 }
             },
+            new ClipboardRule
+            {
+                Label = "DevOps PR",
+                /*language=regex*/
+                RegexPattern = @"
+                    (?i)
+                    ^
+                    \s*
+                    PR\s?
+                    ([0-9]+)
+                    \s*
+                    $
+                    ",
+                QuickActions = new List<QuickAction>()
+                {
+                    new QuickAction
+                    {
+                        CanCopy = true,
+                        Name = "Pull Request",
+                        OpenLabel = "View Pull Request",
+                        Url = "https://neovici.visualstudio.com/Cosmoz3/Cosmoz3%20Team/_git/cz3backend/pullrequest/{1}",
+                    }
+                }
+            },
+            new ClipboardRule
+            {
+                Label = "Guid",
+                /*language=regex*/
+                RegexPattern = @"
+                    (?im)                       
+                    ^
+                    \s*
+                    [{(]?
+                    [0-9A-F]{8}
+                    [-]?
+                    (?:[0-9A-F]{4}[-]?){3}
+                    [0-9A-F]{12}
+                    [)}]? 
+                    \s*
+                    $",
+                QuickActions = new List<QuickAction>()
+                {
+                    new QuickAction
+                    {
+                        CanCopy = true,
+                        Name = "Guid",
+                        OpenLabel = "Copy guid",
+                    }
+                }
+            },
         };
 
         public ClipboardRule()
@@ -60,7 +114,7 @@
             QuickActions = new List<QuickAction>();
             Label = "{0}";
         }
-        
+
         public string Label { get; set; }
 
         public string[] Values { get; set; }
@@ -80,7 +134,7 @@
         public virtual bool IsMatch(string input, out string[] output)
         {
             var match = RuleRegex.Match(input);
-            
+
             if (match.Success)
             {
                 output = match.Groups.OfType<Group>().Select(g => g.Value).ToArray();
